a167f646bbe91674dd17299985e3cae0cf3a9786
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:20:23 +0300

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,18 @@
-package com.epam.mjc.collections.list;
-
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> resultList = new LinkedList<>();
+        
+        for (Integer number : sourceList) {
+            if (number % 2 == 0) {
+                resultList.addLast(number); // Четные в конец
+            } else {
+                resultList.addFirst(number); // Нечетные в начало
+            }
+        }
+        
+        return resultList;
     }
 }
