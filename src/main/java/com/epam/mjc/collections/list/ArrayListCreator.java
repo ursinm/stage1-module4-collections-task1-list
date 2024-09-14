3e35102531c3f5e757fe4f6099317961516d5e9b
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:19:51 +0300

    Update ArrayListCreator.java

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -1,9 +1,17 @@
-package com.epam.mjc.collections.list;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> resultList = new ArrayList<>();
+        
+        // Итерируем по списку начиная с 0, но индексами, кратными 3
+        for (int i = 2; i < sourceList.size(); i += 3) {
+            String word = sourceList.get(i);
+            resultList.add(word);
+            resultList.add(word); // Добавляем слово дважды
+        }
+        
+        return resultList;
     }
 }
